0fadf514a29d0d896d0ec3c5f05346b56f93ebe9
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-22 04:19:41 -0300

    feat(web): :wrench: configured providers

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter } from 'react-router-dom';
 import { TrpcProvider } from './trpc';
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <CookiesProvider>
             <TrpcProvider>
-                {children}
+                <BrowserRouter basename="/">
+                    {children}
+                </BrowserRouter>
             </TrpcProvider>
         </CookiesProvider>
     );
