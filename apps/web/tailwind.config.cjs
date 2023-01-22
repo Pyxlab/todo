40c8b94d80a80fdfb3d80cbff0a2a96cc3f4282f
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-21 23:19:33 -0300

    feat(web): :package: add react-router-dom

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                slate: {
+                    800: "#141E33",
+                },
+            },
+            screens: {
+                "2xl": "1736px",
+            },
+        },
+    },
+    plugins: [],
+    darkMode: "class",
 }
