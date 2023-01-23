c012f2935baa1064edf71a8a4f2e7cb3b5f3b70e
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-23 10:54:36 -0300

    feat(api): :children_crossing: i18next for zod

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -13,6 +13,19 @@
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/standalone'
+import i18next from 'i18next'
+import { z } from 'zod'
+import { zodI18nMap } from 'zod-i18n-map'
+import translation from 'zod-i18n-map/locales/pt/zod.json'
+
+i18next.init({
+  lng: 'pt',
+  resources: {
+    pt: { zod: translation },
+  },
+})
+
+z.setErrorMap(zodI18nMap)
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
