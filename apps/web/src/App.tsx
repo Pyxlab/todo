0044cdd2ab4a8d065b1cde4a3d5a76e35c89d328
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-27 13:37:27 -0300

    style(web): :recycle: alter context theme, input dirty styles and add css for react-toastfy

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,39 @@
+import React from "react";
 import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
-import { Aside, Main, SideBar } from "~/templates";
-import { useAuth } from "./context/useAuth";
+import { ToastContainer } from "react-toastify";
+import { Aside, Main, SideNav } from "~/templates";
+import { ToggleTheme } from "./components/ToggleTheme";
+import { useAuth } from "./hooks/useAuth";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
+import { useThemeStore } from "./store";
 
 function App() {
+    const mode = useThemeStore(state => state.mode)
+
+    React.useEffect(() => {
+        const html = document.querySelector<HTMLHtmlElement>("html")!;
+
+        if (mode === "dark") {
+            html.classList.add("dark");
+            document
+                .querySelector('meta[name="theme-color"]')
+                ?.setAttribute("content", "#0f172a");
+        } else {
+            html.classList.remove("dark");
+            document
+                .querySelector('meta[name="theme-color"]')
+                ?.setAttribute("content", "#e2e8f0");
+        }
+
+        return () => {
+            html.classList.remove("dark");
+            document
+                .querySelector('meta[name="theme-color"]')
+                ?.setAttribute("content", "#e2e8f0");
+        };
+    }, [mode]);
+    
     return (
         <div className="bg-slate-200 min-h-screen text-slate-600 dark:bg-slate-900 dark:text-slate-400 xl:text-base sm:text-sm text-xs">
             <Routes>
@@ -20,6 +49,21 @@ function App() {
                     <Route path="/sign-up" element={<SignUp />} />
                 </Route>
             </Routes>
+            <ToastContainer
+                toastClassName="text-slate-600 dark:text-slate-400 bg-slate-100 dark:bg-slate-800"
+                bodyClassName="text-slate-600 dark:text-slate-400 bg-slate-100 dark:bg-slate-800"
+                progressClassName="bg-slate-600 dark:bg-slate-400"
+                position="bottom-left"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme={mode}
+            />
         </div>
     )
 }
@@ -32,8 +76,13 @@ function PublicLayout() {
         return <Navigate to="/" state={{ location }} replace />
     }
 
+    // add toggle theme button top right
+
     return (
         <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="absolute right-4 top-4">
+                <ToggleTheme />
+            </div>
             <Outlet />
         </div>
     )
@@ -47,10 +96,9 @@ function PrivateLayout() {
         return <Navigate to="/sign-in" state={{ location }} replace />
     }
 
-
     return (
         <div className="flex max-xl:flex-col">
-            <SideBar className="max-xl:hidden" />
+            <SideNav className="max-xl:hidden" />
             <Main />
             <Aside className="max-xl:hidden" />
         </div>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Providers } from './providers'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Providers } from "./providers";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Providers>
-        <App />
-    </Providers>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <Providers>
+            <App />
+        </Providers>
+    </React.StrictMode>
+);
