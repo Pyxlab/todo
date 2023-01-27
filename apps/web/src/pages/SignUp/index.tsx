9eb4d02f893160aa5ac09a9eed63598c10371c0f
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-27 13:39:06 -0300

    feat(web): :sparkles: logging in by the backend

--- apps/web/src/pages/SignUp/index.tsx
+++ apps/web/src/pages/SignUp/index.tsx
@@ -2,13 +2,26 @@ import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { NavLink } from "react-router-dom";
 import { Button } from "~/components/Button";
 import { InputField } from "~/components/FormFields";
+import { trpc } from "~/utils/trpc";
+import { AppRouter } from "@acme/api";
+import { inferRouterInputs } from "@trpc/server";
+import { useErrorHandler } from "~/utils/error-handle";
+
+type CreateUserInput = inferRouterInputs<AppRouter>["users"]["create"];
 
 export const SignUp: React.FC = () => {
-    const methods = useForm();
+    const methods = useForm<CreateUserInput>();
 
-    const handleSubmit: SubmitHandler<{}> = (data) => {
-        console.log(data);
-    };
+    const { mutate } = trpc.users.create.useMutation();
+
+    const handleSubmit = methods.handleSubmit((data) => {
+        mutate(data, {
+            onSuccess: (data) => {
+                console.log("data", data);
+            },
+            onError: useErrorHandler(methods.setError)
+        });
+    });
 
     return (
         <FormProvider {...methods}>
@@ -22,7 +35,7 @@ export const SignUp: React.FC = () => {
                         />
                         <h1 className="text-2xl mt-2">TO-DO LIST</h1>
                     </hgroup>
-                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-400">
                         Sign up for free
                     </h2>
                     <p className="mt-2 text-center text-sm text-gray-600">
@@ -35,11 +48,23 @@ export const SignUp: React.FC = () => {
                         </NavLink>
                     </p>
                 </div>
-                <form onSubmit={methods.handleSubmit(handleSubmit)} className="mt-8 space-y-6">
-                    <div className="-space-y-px flex flex-col gap-4">
+                <form
+                    onSubmit={handleSubmit}
+                    className="mt-8 space-y-6"
+                >
+                    <div className="-space-y-px flex flex-col gap-2">
                         <InputField name="name" label="Full name" fullWidth />
-                        <InputField name="email" label="Email address" fullWidth />
-                        <InputField name="password" label="Password" type="password" fullWidth />
+                        <InputField
+                            name="email"
+                            label="Email address"
+                            fullWidth
+                        />
+                        <InputField
+                            name="password"
+                            label="Password"
+                            type="password"
+                            fullWidth
+                        />
                     </div>
 
                     <Button type="submit" className="w-full">
