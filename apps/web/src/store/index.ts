f2228cecb5e8577102569264f55bb32d558d01e3
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-29 13:23:13 -0300

    perf(web): :ambulance: unified store

--- apps/web/src/store/index.ts
+++ apps/web/src/store/index.ts
@@ -7,6 +7,12 @@ interface State {
     setTotal: (total: number) => void;
     sortBy: SortItem;
     handleSortChange: (item: SortItem) => void;
+    sideNavOpen: boolean;
+    toggleSideNav: () => void;
+    asideOpen: boolean;
+    toggleAside: () => void;
+    newTaskOpen: boolean;
+    toggleNewTask: () => void;
 }
 
 export const useStore = create<State>((set) => ({
@@ -14,6 +20,12 @@ export const useStore = create<State>((set) => ({
     sortBy: sortList[0],
     handleSortChange: (item) => set({ sortBy: item }),
     setTotal: (total) => set({ total }),
+    sideNavOpen: false,
+    toggleSideNav: () => set((state) => ({ sideNavOpen: !state.sideNavOpen })),
+    asideOpen: false,
+    toggleAside: () => set((state) => ({ asideOpen: !state.asideOpen })),
+    newTaskOpen: false,
+    toggleNewTask: () => set((state) => ({ newTaskOpen: !state.newTaskOpen })),
 }));
 
 export * from "./useTheme";
