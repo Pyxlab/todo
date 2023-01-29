931423718571b20397f1a132f2891d9224b2fdc9
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-29 13:15:38 -0300

    refactor(web): :lipstick: transation transform icon animation

--- apps/web/src/components/Directories/index.tsx
+++ apps/web/src/components/Directories/index.tsx
@@ -5,16 +5,18 @@ export const Directories: React.FC = () => {
     return (
         <Disclosure>
             {({ open }) => (
-                <div className='py-4'>
-                    <Disclosure.Button
-                        className={`flex items-center w-full mx-4 mb-2 ${open ? "dark:text-slate-200" : ""}`}
-                    >
-                        {open ? <CaretDown className='mr-1 transition-transform' /> : <CaretRight className='mr-1' />}
+                <div className="py-4">
+                    <Disclosure.Button className="flex items-center w-full pl-3 dark:text-slate-200">
+                        <div className={`mr-1 transition-transform ${open ? "transform rotate-90" : ""}`}>
+                            <CaretRight weight="bold" />
+                        </div>
                         Directories
                     </Disclosure.Button>
                     <Disclosure.Panel className="text-gray-500">
                         <ul className="max-h-36 overflow-auto">
-                            <li className='flex items-center pr-4 pl-9 py-2'>Main</li>
+                            <li className="flex items-center pr-4 pl-9 py-2">
+                                Main
+                            </li>
                         </ul>
                     </Disclosure.Panel>
                 </div>
