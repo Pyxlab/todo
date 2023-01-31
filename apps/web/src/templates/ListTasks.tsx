ec346900c45fdbd29d30cfef4717ea9ba8e8b17d
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-31 20:25:45 -0300

    feat(web): :sparkles: new component list memorizing task

---
+++ apps/web/src/templates/ListTasks.tsx
@@ -0,0 +1,84 @@
+import { AppRouter } from "@acme/api";
+import { inferRouterOutputs } from "@trpc/server";
+import React, { memo } from "react";
+import { toast } from "react-toastify";
+import { Task } from "~/components/Task";
+import { queryClient } from "~/providers/trpc";
+import { useStore } from "~/store";
+import { trpc } from "~/utils/trpc";
+
+type Task = inferRouterOutputs<AppRouter["todos"]>["create"];
+
+type ListTasksProps = {
+    tasks: Task[];
+};
+
+export const ListTasks: React.NamedExoticComponent<ListTasksProps> = memo(
+    ({ tasks }) => {
+        const listView = useStore((state) => state.listView);
+        const toggleNewTask = useStore((state) => state.toggleNewTask);
+
+        const { mutate } = trpc.todos.update.useMutation();
+
+        const handleUpdate = (id: string, data: Partial<Task>) => {
+            const transform = {
+                ...data,
+                dueDate: data.dueDate
+                    ? new Date(data.dueDate as any)
+                    : undefined,
+            };
+
+            mutate(
+                { ...transform, id },
+                {
+                    onSuccess: () => {
+                        toast.success("Task updated successfully");
+
+                        queryClient.invalidateQueries({
+                            queryKey: trpc.todos.getByUser.getQueryKey(),
+                        });
+                    },
+                    onError: () => {
+                        toast.error("Something went wrong");
+                    },
+                }
+            );
+        };
+
+        return (
+            <ul
+                className={`
+                mt-4 grid gap-2 sm:gap-4 xl:gap-6
+                ${
+                    listView === "list"
+                        ? "grid-cols-1"
+                        : "2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 items-end"
+                }
+            `}
+            >
+                {tasks.map((task, index) => (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        index={index}
+                        listView={listView}
+                        handleUpdate={handleUpdate}
+                    />
+                ))}
+                <li>
+                    <button
+                        className={`
+                        border-2 border-slate-300 text-slate-400 w-full rounded-lg border-dashed transition
+                        hover:bg-slate-300 hover:text-slate-500
+                        dark:border-slate-700 dark:hover:bg-slate-800 dark:hover:text-slate-300
+                        ${listView === "list" ? "h-20 sm:h-32" : "h-52 sm:h-64"}
+                    `}
+                        onClick={toggleNewTask}
+                    >
+                        Add new task
+                    </button>
+                </li>
+            </ul>
+        );
+    },
+);
