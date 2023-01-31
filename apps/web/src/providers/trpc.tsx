7486653143a3b89fdacbd4b8abd5130a1573fd32
Walaff Fernandes <walaffonofre@icloud.com>
2023-01-31 20:22:35 -0300

    perf(web): :zap: caching fetched data

--- apps/web/src/providers/trpc.tsx
+++ apps/web/src/providers/trpc.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 import { useCookies } from 'react-cookie';
@@ -7,7 +7,15 @@ import { trpc } from '~/utils/trpc';
 
 const url = import.meta.env.VITE_API_URL;
 
-const queryClient = new QueryClient();
+export const queryCache = new QueryCache()
+export const queryClient = new QueryClient({
+    queryCache: queryCache,
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,
+        }
+    }
+});
 
 export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [cookies] = useCookies(['token']);
