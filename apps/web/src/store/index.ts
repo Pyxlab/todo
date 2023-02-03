be9b6d03984da05a0e7c3c9c783a80653953b630
Walaff Fernandes <walaffonofre@icloud.com>
2023-02-03 13:08:11 -0300

    perf(web): :zap: persist partial store

--- apps/web/src/store/index.ts
+++ apps/web/src/store/index.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { SortItem, sortList } from "~/utils/sort-list";
-
 
 interface State {
     total: number;
     setTotal: (total: number) => void;
     sortBy: SortItem;
     handleSortChange: (item: SortItem) => void;
+    directoryId: string | null;
+    handleDirectoryId: (id: string | null) => void;
     sideNavOpen: boolean;
     toggleSideNav: () => void;
     asideOpen: boolean;
@@ -17,20 +19,41 @@ interface State {
     toggleListView: () => void;
 }
 
-export const useStore = create<State>((set) => ({
-    total: 0,
-    sortBy: sortList[0],
-    handleSortChange: (item) => set({ sortBy: item }),
-    setTotal: (total) => set({ total }),
-    sideNavOpen: false,
-    toggleSideNav: () => set((state) => ({ sideNavOpen: !state.sideNavOpen })),
-    asideOpen: false,
-    toggleAside: () => set((state) => ({ asideOpen: !state.asideOpen })),
-    newTaskOpen: false,
-    toggleNewTask: () => set((state) => ({ newTaskOpen: !state.newTaskOpen })),
-    listView: "list",
-    toggleListView: () => set((state) => ({ listView: state.listView === "list" ? "grid" : "list" })),
-}));
+export const useStore = create(
+    persist<State>(
+        (set) => ({
+            total: 0,
+            sortBy: sortList[0],
+            handleSortChange: (item) => set({ sortBy: item }),
+            directoryId: null,
+            handleDirectoryId: (id) => set({ directoryId: id }),
+            setTotal: (total) => set({ total }),
+            sideNavOpen: false,
+            toggleSideNav: () =>
+                set((state) => ({ sideNavOpen: !state.sideNavOpen })),
+            asideOpen: false,
+            toggleAside: () =>
+                set((state) => ({ asideOpen: !state.asideOpen })),
+            newTaskOpen: false,
+            toggleNewTask: () =>
+                set((state) => ({ newTaskOpen: !state.newTaskOpen })),
+            listView: "list",
+            toggleListView: () =>
+                set((state) => ({
+                    listView: state.listView === "list" ? "grid" : "list",
+                })),
+        }),
+        {
+            name: "store",
+            partialize: (state) =>
+                ({
+                    total: state.total,
+                    sortBy: state.sortBy,
+                    listView: state.listView,
+                } as State),
+        }
+    )
+);
 
 export * from "./useTheme";
 export * from "./useRemember";
