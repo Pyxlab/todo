69b59d81d1833a880fbf59de6a665dc2787472ed
Walaff Fernandes <walaffonofre@icloud.com>
2023-02-02 22:03:54 -0300

    refactor(api): :recycle: added filter to query all tasks

--- apps/api/app/Routes/Todo/getTodoByUser.ts
+++ apps/api/app/Routes/Todo/getTodoByUser.ts
@@ -1,6 +1,24 @@
+import { ModelQueryBuilderContract } from '@ioc:Adonis/Lucid/Orm'
 import procedure from '@ioc:Pyxlab/Adonis/Trpc/Procedure'
 import Todo from 'App/Models/Todo'
 import { z } from 'zod'
+
+type TodoQuery = ModelQueryBuilderContract<typeof Todo, Todo>
+
+const sortOrders = {
+  'order-added': 'created_at',
+  'min-date': 'due_date',
+  'max-date': 'due_date',
+  'completed-first': 'completed',
+  'uncompleted-first': 'completed',
+} as const
+
+const filterConditions = {
+  important: (query: TodoQuery) => query.where('important', true),
+  completed: (query: TodoQuery) => query.where('completed', true),
+  uncompleted: (query: TodoQuery) => query.where('completed', false),
+  today: (query: TodoQuery) => query.whereRaw(`DATE(due_date) = CURDATE()`),
+} as const
 
 export const getTodoByUserProcedure = procedure.protected
   .input(
@@ -13,54 +31,33 @@ export const getTodoByUserProcedure = procedure.protected
           'completed-first',
           'uncompleted-first',
         ] as const),
+        filterBy: z.enum(['important', 'completed', 'uncompleted', 'today'] as const).optional(),
       })
       .nullish()
   )
   .query(async ({ ctx, input }) => {
+    const query = Todo.query().where('user_id', ctx.auth.user?.id!).preload('directory')
+
     if (!input) {
-      const todos = await Todo.query().where('user_id', ctx.auth.user?.id!).preload('directory')
+      const todos = await query
 
       return todos.map((todo) => todo.serialize())
     }
 
-    switch (input.sortBy) {
-      case 'order-added':
-        const todos = await Todo.query()
-          .where('user_id', ctx.auth.user?.id!)
-          .orderBy('created_at', 'asc')
-          .preload('directory')
-
-        return todos.map((todo) => todo.serialize())
-      case 'min-date':
-        const todosMinDate = await Todo.query()
-          .where('user_id', ctx.auth.user?.id!)
-          .orderBy('due_date', 'asc')
-          .preload('directory')
-
-        return todosMinDate.map((todo) => todo.serialize())
-
-      case 'max-date':
-        const todosMaxDate = await Todo.query()
-          .where('user_id', ctx.auth.user?.id!)
-          .orderBy('due_date', 'desc')
-          .preload('directory')
-
-        return todosMaxDate.map((todo) => todo.serialize())
-
-      case 'completed-first':
-        const todosCompletedFirst = await Todo.query()
-          .where('user_id', ctx.auth.user?.id!)
-          .orderBy('completed', 'desc')
-          .preload('directory')
-
-        return todosCompletedFirst.map((todo) => todo.serialize())
-
-      case 'uncompleted-first':
-        const todosUncompletedFirst = await Todo.query()
-          .where('user_id', ctx.auth.user?.id!)
-          .orderBy('completed', 'asc')
-          .preload('directory')
-
-        return todosUncompletedFirst.map((todo) => todo.serialize())
+    if (input.filterBy) {
+      filterConditions[input.filterBy](query)
     }
+
+    const sortBy = sortOrders[input.sortBy]
+    let direction: 'asc' | 'desc' = 'asc'
+
+    if (sortBy === 'completed') {
+      direction = 'desc'
+    } else if (input.sortBy === 'max-date') {
+      direction = 'desc'
+    }
+
+    const todos = await query.orderBy(sortBy, direction)
+
+    return todos.map((todo) => todo.serialize())
   })
